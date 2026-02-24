378f867a49ca3bece54659c1b7a9a09ae25b7d21
Ubuntu <ubuntu@ip-172-31-11-83.ap-south-1.compute.internal>
2026-02-24 12:44:03 +0000

    updated code

---
+++ Hellojenkins.java
@@ -0,0 +1,13 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins from Java!");
+        System.out.println("Build trigger successfully!");
+
+    }
+}
+
+// comment
+// test webhook
+
+
+
